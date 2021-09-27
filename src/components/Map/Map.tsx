05e188f02a0e48f5e04ab64da56a0807e6c28836
unknown <kasia.pstrokonska@grupa.it>
2021-09-27 10:52:32 +0200

    Map component

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,33 +1,44 @@
-import React, {FC} from "react";
-import { TileLayer, Marker, Popup } from 'react-leaflet'
-import { MapWrapper, MapComponent, Image, FlexRow, PopUpText } from './MapStyle'
+import React, { FC } from "react";
+import { TileLayer, Marker, Popup } from "react-leaflet";
+import {
+  MapWrapper,
+  MapComponent,
+  Image,
+  FlexRow,
+  PopUpText,
+} from "./MapStyle";
 
 interface MapInterface {
-    image: {
-        source: string,
-        latitude: number,
-        longitude: number
-    }
+  image: {
+    source: string;
+    latitude: number;
+    longitude: number;
+  };
 }
 const Map: FC<MapInterface> = ({ image }) => {
-    let src = image.source
-    let coordinates: [number, number] = [image.latitude, image.longitude]
-    return (
-        <MapWrapper>
-        <MapComponent center={coordinates} zoom={5} scrollWheelZoom={true}>
-          <TileLayer
-            url="https://stamen-tiles.a.ssl.fastly.net/toner/{z}/{x}/{y}.png"
-          />
-          {src !== "img" && 
+  let src = image.source;
+  let coordinates: [number, number] = [image.latitude, image.longitude];
+  return (
+    <MapWrapper>
+      <MapComponent center={[-10, 30]} zoom={5} scrollWheelZoom={true}>
+        <TileLayer url="https://stamen-tiles.a.ssl.fastly.net/toner/{z}/{x}/{y}.png" />
+        {src !== "img" && (
           <Marker position={coordinates}>
             <Popup>
-              <FlexRow><Image src={src} alt="Loading failed"/><div><PopUpText>Coordinates:</PopUpText><PopUpText>{coordinates[0]}</PopUpText><PopUpText> {coordinates[1]}</PopUpText></div></FlexRow>
+              <FlexRow>
+                <Image src={src} alt="Loading failed" />
+                <div>
+                  <PopUpText>Coordinates:</PopUpText>
+                  <PopUpText>{coordinates[0]}</PopUpText>
+                  <PopUpText> {coordinates[1]}</PopUpText>
+                </div>
+              </FlexRow>
             </Popup>
-          </Marker> }
-          
-        </MapComponent>
-      </MapWrapper>
-    )
-}
+          </Marker>
+        )}
+      </MapComponent>
+    </MapWrapper>
+  );
+};
 
-export default Map
+export default Map;
