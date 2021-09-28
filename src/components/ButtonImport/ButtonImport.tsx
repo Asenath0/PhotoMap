4874a4e26d30ffbf0e33f917d670521f9f5384f4
Kasia Pstrokońska <kasia.pstrokonska@grupa.it>
2021-09-28 09:22:26 +0000

    Main page

--- src/components/ButtonImport/ButtonImport.tsx
+++ src/components/ButtonImport/ButtonImport.tsx
@@ -1,20 +1,33 @@
-import React from "react";
-import { Button, Icon } from "./ButtonImportStyle";
-import { useState } from "react";
-import ImportField from "../ImportField/ImportField";
+import React, {FC} from "react";
+import { Button, Icon, Wrapper, ImageWrapper, CarouselWrapper } from "./ButtonImportStyle";
+import { Carousel } from '@trendyol-js/react-carousel';
 
-const ButtonImport = () => {
-  const [isImportingOn, setIsImportingOn] = useState(false);
+interface ButtonImportInterface {
+  ImportingHandler: () => void;
+  shouldAnimate: (bool: boolean) => void;
+}
 
-  const isImportingOnHandler = (): void => setIsImportingOn(isImportingOn => !isImportingOn);
-
+const ButtonImport: FC<ButtonImportInterface> = ({ ImportingHandler, shouldAnimate }) => {
   return (
-    <div>
-      {isImportingOn && (
-        <ImportField closeImportingView={isImportingOnHandler} />
-      )}
-      <Button onClick={isImportingOnHandler}><Icon /><p>Import Image</p></Button>
-    </div>
+    <Wrapper>
+      <Button onClick={() => {ImportingHandler(); shouldAnimate(true)}}><Icon /><p>Import Image</p></Button>
+
+      <CarouselWrapper >
+      <Carousel show={10} slide={3} swiping={true}>
+        <ImageWrapper style={{backgroundImage: 'url("https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg")'}} />
+        <ImageWrapper style={{backgroundImage: 'url("https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg")'}} />
+        <ImageWrapper style={{backgroundImage: 'url("https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg")'}} />
+        <ImageWrapper style={{backgroundImage: 'url("https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg")'}} />
+        <ImageWrapper style={{backgroundImage: 'url("https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg")'}} />
+        <ImageWrapper style={{backgroundImage: 'url("https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg")'}} />
+        <ImageWrapper style={{backgroundImage: 'url("https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg")'}} />
+        <ImageWrapper style={{backgroundImage: 'url("https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg")'}} />
+        <ImageWrapper style={{backgroundImage: 'url("https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg")'}} />
+      
+      </Carousel>
+      </CarouselWrapper>
+      
+    </Wrapper>
   );
 };
 
--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,15 +1,150 @@
-import { useContext } from "react";
-import AuthContext from "../../store/auth-context";
+import Map from "../Map/Map";
+import ImportField from "../ImportField/ImportField";
+import { useState, useRef } from "react";
+import Header from "../Header/Header";
+import ImportFieldAnimation from "../ImportField/ImportFieldAnimation";
+import Footer from "../Footer/Footer";
+import EXIF from "exif-js";
+import ButtonImport from "../ButtonImport/ButtonImport";
 
 const Dashboard = () => {
-  const authContext = useContext(AuthContext);
+  const [shouldAnimate, setShouldAnimate] = useState(false);
+  const shouldAnimateHandler = (bool: boolean) => setShouldAnimate(bool);
+
+  const [isImportingOn, setIsImportingOn] = useState(false);
+
+  const isImportingOnHandler = (): void =>
+    setIsImportingOn((isImportingOn) => !isImportingOn);
+
+  const [currentImage, setCurrentImage] = useState({
+    source: "",
+    longitude: 0,
+    latitude: 0,
+    status: "Please load image",
+  });
+
+  const [coordinates, setCoordinates] = useState<[number, number]>([
+    currentImage.latitude,
+    currentImage.longitude,
+  ]);
+
+  const coordinatesHandler = () =>
+    setCoordinates([currentImage.latitude, currentImage.longitude]);
+
+  const inputRef: React.Ref<HTMLInputElement> = useRef(null);
+
+  const ConvertDMSToDD = (
+    degrees: number,
+    minutes: number,
+    seconds: number,
+    direction: string
+  ) => {
+    let dd = degrees + minutes / 60 + seconds / (60 * 60);
+
+    (direction === "S" || direction === "W") && (dd = dd * -1);
+
+    return dd;
+  };
+
+  const currentImageHandler = (): void => {
+    //@ts-ignore
+    let img = inputRef.current.files[0];
+
+    if (img !== null && img !== undefined) {
+      const fileType = img["type"];
+      const validImageTypes = [
+        "image/gif",
+        "image/jpg",
+        "image/png",
+        "image/jpeg",
+      ];
+
+      if (validImageTypes.includes(fileType)) {
+        let src: string = URL?.createObjectURL(img);
+        //@ts-ignore
+        EXIF.getData(img, function () {
+          let getLongitude = EXIF.getTag(img, "GPSLongitude");
+          let getLatitude = EXIF.getTag(img, "GPSLatitude");
+          let getLongitudeTag = EXIF.getTag(img, "GPSLongitudeRef");
+          let getLatitudeTag = EXIF.getTag(img, "GPSLatitudeRef");
+
+          if (getLongitude === undefined || getLatitude === undefined) {
+            setCurrentImage({
+              source: "",
+              longitude: 0,
+              latitude: 0,
+              status: "Sorry, this image has no GPS information",
+            });
+          } else {
+            let longitude = `${getLongitude}`;
+            let latitude = `${getLatitude}`;
+
+            let longitudeTab = longitude.split(",");
+            let latitudeTab = latitude.split(",");
+
+            src &&
+              setCurrentImage({
+                source: src,
+                latitude: ConvertDMSToDD(
+                  parseInt(latitudeTab[0]),
+                  parseInt(latitudeTab[1]),
+                  parseInt(latitudeTab[2]),
+                  getLatitudeTag
+                ),
+                longitude: ConvertDMSToDD(
+                  parseInt(longitudeTab[0]),
+                  parseInt(longitudeTab[1]),
+                  parseInt(longitudeTab[2]),
+                  getLongitudeTag
+                ),
+                status: "Loaded",
+              });
+          }
+        });
+      } else {
+        setCurrentImage({
+          source: "",
+          longitude: 0,
+          latitude: 0,
+          status: "Wrong file extention, please use .jpeg, .jpg, .png or .gif",
+        });
+      }
+    } else {
+      setCurrentImage({
+        source: "",
+        longitude: 0,
+        latitude: 0,
+        status: "Loading image failed",
+      });
+    }
+  };
 
   return (
     <div>
-      <h1>Dashboard</h1>
-      <h2>{authContext.name}</h2>
-      <h2>{authContext.userId}</h2>
-      <button onClick={authContext.logout}>Logout</button>
+      {shouldAnimate && (
+        <ImportFieldAnimation
+          isImportingOn={isImportingOn}
+          child={
+            <ImportField
+              isImportingOnHandler={isImportingOnHandler}
+              inputRef={inputRef}
+              currentImage={currentImage}
+              currentImageHandler={currentImageHandler}
+              coordinatesHandler={coordinatesHandler}
+            />
+          }
+        />
+      )}
+
+      <Header />
+      <Map currentImage={currentImage.source} coordinates={coordinates} />
+
+      <ButtonImport
+        ImportingHandler={isImportingOnHandler}
+        shouldAnimate={shouldAnimateHandler}
+      />
+
+      <Footer />
     </div>
   );
 };
